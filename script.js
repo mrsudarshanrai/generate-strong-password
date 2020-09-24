76c7100b44b0ad493bde0a8cd019ef11f8edff0f
mrsudarshan <rrraishati@gmail.com>
2020-09-25 02:16:35 +0545

    user specified password length

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function generatePassword() {
-  let length = 8;
-  let chars = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()`;
-  chars = chars.replace('\n', " ").replace(" ", '');
-  let password = "";
-  let n = chars.length;
-  for (var i = 0; i < length; ++i) {
-      password += chars.charAt(Math.floor(Math.random() * n));
+password ={
+  generate: arg =>{
+
+      let length = arg || 8;
+      let chars = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()`;
+      chars = chars.replace('\n', " ").replace(" ", '');
+      let password = "";
+      let n = chars.length;
+      for (var i = 0; i < length; ++i) {
+          password += chars.charAt(Math.floor(Math.random() * n));
+      }
+      console.log(password)
   }
-  alert(password)
+}
+
+const generatePassword = ()=>{
+  password.generate(8);
 }
